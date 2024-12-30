26c83c998d44a8e6c9cbd5b41a177871db28c107
ashwin071203 <164607519+ashwin071203@users.noreply.github.com>
2024-12-31 00:08:33 +0530

    java programmes

--- src/arithmetic_operators.java
+++ src/arithmetic_operators.java
@@ -1,8 +1,10 @@
 public class arithmetic_operators {
     public static void main(String[] args) {
-        int a = 2;//Unary
-
+        int a = 2; //unary
+        System.out.println(a++);
+        int b = 3;
+        System.out.println(++b);
+        int c = a+3;
         System.out.println(c);
-
     }
 }
